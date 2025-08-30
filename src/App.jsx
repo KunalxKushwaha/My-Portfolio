97c5462ab98c5778548f00cf750f816a31a2177c
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-08-30 19:10:54 +0530

    Company Bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from './sections/Hero'
 import Navbar from './components/Navbar'
 import ShowcaseSection from './sections/ShowcaseSection'
+import LogoSection from './components/LogoSection'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
    <Navbar/>
    <Hero/>
    <ShowcaseSection/>
+   <LogoSection/>
    </>
   )
 }
