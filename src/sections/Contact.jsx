f85cf24e009ecd846f23c239047fd08354453354
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-09-02 13:55:11 +0530

    Almost Done

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,6 +1,5 @@
 import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
-
 import TitleHeader from "../components/TitleHeader.jsx";
 import ContactExperience from "../components/ContactExperience.jsx";
 
@@ -109,7 +108,8 @@ const Contact = () => {
             </div>
           </div>
 
-          <div className="xl:col-span-7 min-h-96">
+          <div
+          className="xl:col-span-7 min-h-96">
             <div className="bg-[#cd7c2e] w-full h-full hover:cursor-grab rounded-3xl overflow-hidden">
               <ContactExperience />
             </div>
